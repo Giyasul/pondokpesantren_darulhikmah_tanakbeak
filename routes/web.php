87b77daf6a250c2371408555a23603cde3a700a6
Giyasul Firdaus <158206218+Giyasul@users.noreply.github.com>
2026-02-13 00:54:19 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,4 @@ Route::get('/', [BeritaController::class, 'index']);
 Route::get('/galeri', [GaleriController::class, 'index']);
 Route::get('/berita/{id}', [BeritaController::class, 'show'])->name('berita');
 Route::get('/berita', [BeritaController::class, 'semua'])->name('berita.semua');
-
-
 Route::get('/perpustakaan', [PerpustakaanController::class, 'index'])->name('perpustakaan.index');
