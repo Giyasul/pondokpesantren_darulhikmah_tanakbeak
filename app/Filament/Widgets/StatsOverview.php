dd7c4ebbb6159bb6de4cb6e0408fdc571a52db99
Giyasul Firdaus <158206218+Giyasul@users.noreply.github.com>
2026-02-12 14:17:42 +0800

    day 15

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Berita;
+use App\Models\Galeri;
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Berita', Berita::count())
+                ->description('Jumlah berita')
+                ->descriptionIcon('heroicon-m-newspaper')
+                ->color('success'),
+
+            Stat::make('Total Galeri', Galeri::count())
+                ->description('Jumlah galeri')
+                ->descriptionIcon('heroicon-m-photo')
+                ->color('info'),
+
+            Stat::make('Total User', User::count())
+                ->description('Jumlah user')
+                ->descriptionIcon('heroicon-m-users')
+                ->color('danger'),
+        ];
+    }
+}
